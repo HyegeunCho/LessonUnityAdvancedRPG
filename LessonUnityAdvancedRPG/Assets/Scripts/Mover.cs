4d07a83dad9da1b51e1e500e6e3cf15d527de871
조혜근 <hgcho84.kr@gmail.com>
2024-03-07 08:27:23 +0900

    [Update] 플레이어 애니메이션 추가

--- LessonUnityAdvancedRPG/Assets/Scripts/Mover.cs
+++ LessonUnityAdvancedRPG/Assets/Scripts/Mover.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
 public class Mover : MonoBehaviour
 {
     private NavMeshAgent _agent;
+    private Animator _animator;
 
     void Start()
     {
         _agent = transform.GetComponent<NavMeshAgent>();
+        _animator = transform.GetComponent<Animator>();
     }
 
     void Update()
@@ -17,6 +20,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -26,5 +30,16 @@ public class Mover : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (!Physics.Raycast(ray, out RaycastHit hit)) return;
         _agent.SetDestination(hit.point);
+    }
+    
+    private void UpdateAnimator()
+    {
+        /// _agent.velocity는 월드에서의 유닛의 위치와 목적지에 대한 속도를 갖고 있음
+        /// 애니메이터는 플레이어 자체 (로컬)에 속해 있고 플레이어 자체의 전진 속도만 필요하기 때문에
+        /// 로컬로 변환해서 사용하는 것. 
+        
+        // Global을 Local로 변환 처리
+        var localVelocity = transform.InverseTransformDirection(_agent.velocity);
+        _animator.SetFloat("forwardSpeed", localVelocity.z);
     }
 }
