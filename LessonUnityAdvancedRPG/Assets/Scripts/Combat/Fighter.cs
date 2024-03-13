6d59fa9e5233bb62bbc9ed731df1aacb9353547d
조혜근 <hgcho84.kr@gmail.com>
2024-03-13 13:12:25 +0900

    [Update] 공격 대상으로 이동 처리

--- LessonUnityAdvancedRPG/Assets/Scripts/Combat/Fighter.cs
+++ LessonUnityAdvancedRPG/Assets/Scripts/Combat/Fighter.cs
@@ -1,14 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
+        [SerializeField] private float weaponRange = 2f;
+        
+        private Transform target;
+
+        private void Update()
+        {
+            if (target != null && Vector3.Distance(target.position, transform.position) > weaponRange)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
+        }
+
         public void Attack(CombatTarget inTarget)
         {
-            Debug.LogError($"Take that you shor, squat peasant!");
+            target = inTarget.transform;
         }
     }
 }
