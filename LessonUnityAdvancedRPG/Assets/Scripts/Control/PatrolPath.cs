07411c64ce6965bf465369263a345f97d98570a8
조혜근 <hgcho84.kr@gmail.com>
2024-03-26 08:37:45 +0900

    [Add] 웨이포인트 기즈모 추가

---
+++ LessonUnityAdvancedRPG/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private const float waypointGizmoRadius = 0.3f;
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var current = GetWaypoint(i);
+                Gizmos.DrawSphere(current, waypointGizmoRadius);
+                var next = GetNextWaypoint(i);
+                Gizmos.DrawLine(current, next);
+            }
+        }
+
+        public Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i % transform.childCount).position;
+        }
+
+        public Vector3 GetNextWaypoint(int i)
+        {
+            return GetWaypoint(GetNextIndex(i));
+        }
+
+        private int GetNextIndex(int inCurrentIndex)
+        {
+            return (inCurrentIndex + 1) % transform.childCount;
+        }
+    }
+    
+}
