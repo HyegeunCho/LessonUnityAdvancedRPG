5c04b717e339b8f909e8fae4d8fc5894f0349482
조혜근 <hgcho84.kr@gmail.com>
2024-04-22 13:54:07 +0900

    [Add] 포탈 별 Spawn 포인트 생성

--- LessonUnityAdvancedRPG/Assets/Scripts/SceneManagement/Portal.cs
+++ LessonUnityAdvancedRPG/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
@@ -9,6 +12,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int SceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return;
@@ -21,7 +25,37 @@ namespace RPG.SceneManagement
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(SceneToLoad);
             Debug.LogError($"Scene {SceneToLoad} Loaded!");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal target)
+        {
+            var player = GameObject.FindWithTag("Player");
+            var agent = player.GetComponent<NavMeshAgent>();
+            agent.Warp(target.spawnPoint.position);
+            player.transform.rotation = target.spawnPoint.rotation;
+
+            // agent.enabled = false;
+            // player.transform.position = target.spawnPoint.position;
+            // player.transform.rotation = target.spawnPoint.rotation;
+            // agent.enabled = true;
+
+        } 
+
+        private Portal GetOtherPortal()
+        {
+            var portals = GameObject.FindObjectsByType<Portal>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+            foreach (var portal in portals)
+            {
+                if (portal == this) continue;
+                return portal;
+            }
+
+            return null;
         }
     }
     
